4fb42cf81adb16aa5954073cb37943121055e1b0
John Arley <johnarleycano@hotmail.com>
2018-07-25 09:02:25 -0500

    style(Reportes): limpieza de interfaz
    - Campos organizados
    - Interfaz modificada
    - Código depurado

--- reportes.php
+++ reportes.php
@@ -7,240 +7,201 @@ if (!isset($_SESSION[APL])  || $_SESSION[APL]->usuario->id == "" ){
 	header("location:entrada_usuario.php");
 }
 
-
 if(isset($_GET["msg"]))
 	echo $_SESSION[APL]->msg($_GET["msg"]);
 
 echo $_SESSION[APL]->cabeceras();
+
 $_SESSION[APL]->pagina_menu='reportes.php';
+
 if($_SESSION[APL]->usuario->id_perfil==3)
 	echo $_SESSION[APL]->interfas->pestana(3);
 else
 	echo $_SESSION[APL]->interfas->pestana(6);
-
 ?>
 
-<script>
-function filtrar(tipo)
-{
-	if(document.incidente.fecha_inicio.value=='' || document.incidente.fecha_final.value=='')
-		alert('Seleccione el Periodo')
-	else
-	/*if(tipo==1 &&   document.incidente.id_ambulancia.value=='')
+<script type="text/javascript">
+	function filtrar(tipo)
 	{
-		alert('Seleccione la Ambulancia para generar el Reporte');
-		document.incidente.id_ambulancia.focus();
-	}
-	else
-	if(tipo==2 &&   document.incidente.id_grua.value=='')
-	{
-		alert('Seleccione la Grua para generar el Reporte');
-		document.incidente.id_grua.focus();
-	}
-	else*/
-	{
-		if(tipo==1)
-			document.incidente.action='reportes_excel.php';
-		else
-			document.incidente.action='reportes_excel_2.php';
-	
-		document.incidente.submit();
-	}
-	
-}
+		if(document.incidente.fecha_inicio.value=='' || document.incidente.fecha_final.value==''){
+			alert('Seleccione el Periodo')
 
-function generar(){
-	
-	if(document.incidente.fecha_inicio.value=='' || document.incidente.fecha_final.value==''){
-		alert('Seleccione el Periodo')
-	
-	}else{
-	
-	
-		document.incidente.action='reportes_excel_3.php';
-		
-	
+			return false
+		}
+
+		document.incidente.action = (tipo == 1) ? "reportes_excel.php" : "reportes_excel_2.php"
 		document.incidente.submit();
 	}
-	
-}
+
+	function generar(){
+		if(document.incidente.fecha_inicio.value=='' || document.incidente.fecha_final.value==''){
+			alert('Seleccione el Periodo')
+
+			return false
+		}		
+		
+		document.incidente.action='reportes_excel_3.php';
+		document.incidente.submit();
+	}
 </script>
+
 <script type="text/javascript" src="libs/jq/jquery.min.js"></script>
 <script type="text/javascript" src="libs/jq/ui/js/jquery-ui-1.10.3.custom.min.js"></script>
 <link type="text/css" href="libs/jq/ui/css/custom-theme/jquery-ui-1.10.3.custom.css" rel="stylesheet"/>
 <script type="text/javascript" src="libs/js/vista.js"></script>
+
+<style type="text/css">
+	.zona1 {
+		/*border: 1px solid black;*/
+		display: inline-block;
+		font-family: Tahoma;
+		margin: 0;
+		padding: 0;
+		width: 45%;
+	}
+</style>
+
 <form name="incidente" method="post" action="reportes_excel.php" target="_blank">
-<center>
+	<h1 class="cab_grid">REPORTES</h1>
 
-<table width="100%">
-<tr><th class="cab_grid" colspan="3">
-Reporte por Ambulancia
- </th>
-</tr>
-<tr>
+	<div class="zona1">
+		<!-- Período inicial -->
+		<div class="zona1 resaltar">
+			<strong>PER&Iacute;ODO INICIAL</strong>
+		</div>
+		<div class="zona1">
+			<input class="campos cmpFec" type="text" id="fecha_inicio" name="fecha_inicio" maxlength="10" size="12" value="<?php if(isset($_POST['fecha_inicio'])) echo $_POST['fecha_inicio']; ?>"/>
 
-<th class="resaltar">Periodo Inicial</th>
-<td  align="left" >
+			<select name="horai">
+				<?php for ($h=0;$h<24;$h++)
+				{
+					echo "<option value='".str_pad($h,2,"0",STR_PAD_LEFT)."' ";
+					if(isset($_POST['horai']) && str_pad($h,2,"0",STR_PAD_LEFT)==$_POST['horai'])
+						echo "selected";
+					echo ">".str_pad($h,2,"0",STR_PAD_LEFT)."</option>"; 
+				}
+			?>
+			</select> :
+			
+			<select name="minui">
+				<?php for ($m=0;$m<60;$m++)
+				{
+					echo "<option value='".str_pad($m,2,"0",STR_PAD_LEFT)."' ";
+					if(isset($_POST['minui']) && str_pad($m,2,"0",STR_PAD_LEFT)==$_POST['minui'])
+						echo "selected";
+					echo ">".str_pad($m,2,"0",STR_PAD_LEFT)."</option>"; 
+				}
+				?>
+			</select>
+		</div>
 
-<input class="campos cmpFec" type="text" id="fecha_inicio" name="fecha_inicio" maxlength="10" size="12" value="<?php if(isset($_POST['fecha_inicio'])) echo $_POST['fecha_inicio']; ?>"/>
-	<select name="horai">
-	<?php for ($h=0;$h<24;$h++)
-	{
-	
-		echo "<option value='".str_pad($h,2,"0",STR_PAD_LEFT)."' ";
-		if(isset($_POST['horai']) && str_pad($h,2,"0",STR_PAD_LEFT)==$_POST['horai'])
-			echo "selected";
-		echo ">".str_pad($h,2,"0",STR_PAD_LEFT)."</option>"; 
-	}
-	?>
-	</select> :
-				<select name="minui">
-	<?php for ($m=0;$m<60;$m++)
-	{
-	
-		echo "<option value='".str_pad($m,2,"0",STR_PAD_LEFT)."' ";
-		if(isset($_POST['minui']) && str_pad($m,2,"0",STR_PAD_LEFT)==$_POST['minui'])
-			echo "selected";
-		echo ">".str_pad($m,2,"0",STR_PAD_LEFT)."</option>"; 
-	}
-	?>
-	</select>
-</td>
-<td>&nbsp;</td>
-<tr>
-<th class="resaltar">Fecha Final</th>
-<td>
-<input class="campos cmpFec" type="text" id="fecha_final" name="fecha_final" maxlength="10" size="12" value="<?php 
-if(isset($_POST['fecha_final']))
-{
-		echo $_POST['fecha_final'];
+		<!-- Fecha final -->
+		<div class="zona1 resaltar">
+			<strong>FECHA FINAL</strong>
+		</div>
+		<div class="zona1">
+			<input class="campos cmpFec" type="text" id="fecha_final" name="fecha_final" maxlength="10" size="12" value="<?php if(isset($_POST['fecha_final'])){ echo $_POST['fecha_final']; } ?>"/>
+
+			<select name="horaf">
+				<?php for ($h=0;$h<24;$h++)
+				{
+					echo "<option value='".str_pad($h,2,"0",STR_PAD_LEFT)."' ";
+					if(isset($_POST['horaf']) && str_pad($h,2,"0",STR_PAD_LEFT)==$_POST['horaf'])
+						echo "selected";
+					echo ">".str_pad($h,2,"0",STR_PAD_LEFT)."</option>"; 
+				}
+				?>
+			</select> :
+			<select name="minuf">
+				<?php for ($m=0;$m<60;$m++)
+				{
+					echo "<option value='".str_pad($m,2,"0",STR_PAD_LEFT)."' ";
+					if(isset($_POST['minuf']) && str_pad($m,2,"0",STR_PAD_LEFT)==$_POST['minuf'])
+						echo "selected";
+					echo ">".str_pad($m,2,"0",STR_PAD_LEFT)."</option>"; 
+				}
+				?>
+			</select>
+		</div>
 		
-	}
-	?>"/>
-	<select name="horaf">
-	<?php for ($h=0;$h<24;$h++)
-	{
-	
-		echo "<option value='".str_pad($h,2,"0",STR_PAD_LEFT)."' ";
-		if(isset($_POST['horaf']) && str_pad($h,2,"0",STR_PAD_LEFT)==$_POST['horaf'])
-			echo "selected";
-		echo ">".str_pad($h,2,"0",STR_PAD_LEFT)."</option>"; 
-	}
-	?>
-	</select> :
-				<select name="minuf">
-	<?php for ($m=0;$m<60;$m++)
-	{
-	
-		echo "<option value='".str_pad($m,2,"0",STR_PAD_LEFT)."' ";
-		if(isset($_POST['minuf']) && str_pad($m,2,"0",STR_PAD_LEFT)==$_POST['minuf'])
-			echo "selected";
-		echo ">".str_pad($m,2,"0",STR_PAD_LEFT)."</option>"; 
-	}
-	?>
-	</select>
-</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<th class="resaltar" >Ambulancia</th>
-<td>
-<select name="id_ambulancia" class="campos">
-<option value="">Todas</option>
+		<!-- Ambulancia -->
+		<div class="zona1 resaltar">
+			<strong>AMBULANCIA</strong>
+		</div>
+		<div class="zona1">
+			<select name="id_ambulancia" class="campos">
+				<option value="">Todas</option>
+				<?php
+				$sql="SELECT id,nombre FROM ".$_SESSION[APL]->bd->nombre_bd[0].".dvm_ambulancia ORDER BY id";
+				$amb=$_SESSION[APL]->bd->getRs($sql);
+				while (!$amb->EOF) {
+				   	echo "<option value='".$amb->fields[0]."' ";
+					if(isset($_POST['id_ambulancia']) && $_POST['id_ambulancia']==$amb->fields[0])
+					{
+					
+					
+						echo "selected";
+						 $ambulancia_sel=strtoupper($amb->fields[1]);
+					}
+					else
+						$ambulancia_sel="NO SE HA SELECCIONADO AMBULANCIA";
+					echo ">".$amb->fields[1]."</option>";
+				    $amb->MoveNext();
+				}
+				?>
+			</select>
+		</div>
 
-<?php
+		<!-- Grúa -->
+		<div class="zona1 resaltar">
+			<strong>GR&Uacute;A</strong>
+		</div>
+		<div class="zona1">
+			<select name="id_grua" class="campos">
+				<option value="">Todas</option>
+				<?php
+				$sql="SELECT id,nombre FROM ".$_SESSION[APL]->bd->nombre_bd[0].".dvm_grua ORDER BY id";
+				$amb=$_SESSION[APL]->bd->getRs($sql);
+				while (!$amb->EOF) {
+				   	echo "<option value='".$amb->fields[0]."' ";
+					if(isset($_POST['id_grua']) && $_POST['id_grua']==$amb->fields[0])
+					{
+						echo "selected";
+					}
 
-$sql="SELECT id,nombre FROM ".$_SESSION[APL]->bd->nombre_bd[0].".dvm_ambulancia ORDER BY id";
-$amb=$_SESSION[APL]->bd->getRs($sql);
-while (!$amb->EOF) {
-   	echo "<option value='".$amb->fields[0]."' ";
-	if(isset($_POST['id_ambulancia']) && $_POST['id_ambulancia']==$amb->fields[0])
-	{
-	
-	
-		echo "selected";
-		 $ambulancia_sel=strtoupper($amb->fields[1]);
-	}
-	else
-		$ambulancia_sel="NO SE HA SELECCIONADO AMBULANCIA";
-	echo ">".$amb->fields[1]."</option>";
-    $amb->MoveNext();
-}
+					echo ">".$amb->fields[1]."</option>";
+				    $amb->MoveNext();
+				}
+				?>
+			</select>
+		</div>
 
-?>
-</select></td>
-<td><?php 
-echo $_SESSION[APL]->getButtom('.','Generar Reporte por Ambulancia', '200', 'onclick="filtrar(1)"');
-?>
-</td>
-</tr>
-<tr>
-<th class="resaltar" >Grua</th>
-<td>
-<select name="id_grua" class="campos">
-<option value="">Todas</option>
-<?php
+	</div>
 
-$sql="SELECT id,nombre FROM ".$_SESSION[APL]->bd->nombre_bd[0].".dvm_grua ORDER BY id";
-$amb=$_SESSION[APL]->bd->getRs($sql);
-while (!$amb->EOF) {
-   	echo "<option value='".$amb->fields[0]."' ";
-	if(isset($_POST['id_grua']) && $_POST['id_grua']==$amb->fields[0])
-	{
-	
-	
-		echo "selected";
-		
-	}
+	<div class="zona1">
+		<div class="zona1">
+			<?php 
+			echo $_SESSION[APL]->getButtom('.','Generar Reporte por Ambulancia', '200', 'onclick="filtrar(1)"');
+			echo $_SESSION[APL]->getButtom('.','Generar Reporte por Grua', '200', 'onclick="filtrar(2)"');
 
-	echo ">".$amb->fields[1]."</option>";
-    $amb->MoveNext();
-}
+			$sql="SELECT id, codigo FROM ".$_SESSION[APL]->bd->nombre_bd[0].".dvm_incidente ORDER BY id";
+			$amb=$_SESSION[APL]->bd->getRs($sql);
 
-?>
-</select></td>
-<td><?php 
-echo $_SESSION[APL]->getButtom('.','Generar Reporte por Grua', '200', 'onclick="filtrar(2)"');
-?>
-</td>
-</tr>
-
-<tr>
-<th class="resaltar" ></th>
-<td>
-
-<?php
-
-$sql="SELECT id, codigo FROM ".$_SESSION[APL]->bd->nombre_bd[0].".dvm_incidente ORDER BY id";
-$amb=$_SESSION[APL]->bd->getRs($sql);
-
-?>
-
-</td>
-<td><?php 
-echo $_SESSION[APL]->getButtom('.','Generar Reporte Global', '200', 'onclick="generar()"');
-?>
-</td>
-</tr>
-<tr>
-<th colspan="3" height="10">&nbsp;</th>
-</tr>
-</table>
-
-	<table>
-		<tr>
-			<td><?php echo $_SESSION[APL]->getButtom('.','Grafico Numero de Accidentes por Fecha', '200', 'onclick="verGrafico(1)"'); ?></td>
-			<td><?php echo $_SESSION[APL]->getButtom('.','Grafico Numero de Accidentes por Tramo', '200', 'onclick="verGrafico(2)"'); ?></td>
-		</tr>
-	</table>
-</center>
+			echo $_SESSION[APL]->getButtom('.','Generar Reporte Global', '200', 'onclick="generar()"');
+			?>
+		</div>
+		<div class="zona1">
+			<?php echo $_SESSION[APL]->getButtom('.','Grafico Numero de Accidentes por Fecha', '200', 'onclick="verGrafico(1)"'); ?>
+			<?php echo $_SESSION[APL]->getButtom('.','Grafico Numero de Accidentes por Tramo', '200', 'onclick="verGrafico(2)"'); ?>
+		</div>
+	</div>
 </form>
+
 <div id="venVerGrafico" style="display:none">
 	<center>
 		<iframe id="ifrVerGra" style="border:0px"></iframe>
 	</center>
 </div>
-</body>
 <script>
 	vis_ponCampoFecha(".cmpFec");
 	vis_PonVentana('venVerGrafico','VER GRAFICO',700,1000);
